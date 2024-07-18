890ead9a03b6f88b21214497f9f09867fce391c6
AbdelrahmanAlsadder <aboodalsadder@gmail.com>
2024-07-18 19:49:00 +0300

    added 2 new pages

---
+++ src/layouts/cases/data/DisputePage.js
@@ -0,0 +1,18 @@
+// src/DisputePage.js
+import React from 'react';
+import './dispute.css';
+
+const DisputePage = () => {
+  return (
+    <div className="dispute-container">
+      <h1>Choose Dispute Type</h1>
+      <div className="options-container">
+        <button className="option-button">Beneficiary Account Not Credited</button>
+        <button className="option-button">Misdirected Payment</button>
+        <button className="option-button">Alias Mobile Number is Used by the Wrong Customer</button>
+      </div>
+    </div>
+  );
+};
+
+export default DisputePage;
