c424d2a3abd5ac1f7ef39480d63ad17b5255f10c
AbdelrahmanAlsadder <aboodalsadder@gmail.com>
2024-07-18 19:29:49 +0300

    Created something

--- src/routes.js
+++ src/routes.js
@@ -38,6 +38,9 @@ Coded by www.creative-tim.com
 // Material Dashboard 2 React layouts
 import Dashboard from "layouts/dashboard";
 import Cases from "layouts/cases";
+import Profile from "layouts/profile";
+import SignIn from "layouts/authentication/sign-in";
+
 // @mui icons
 import Icon from "@mui/material/Icon";
 
