184eae9c15cd6dac1c241247bc7b3f1cfd55c90a
AbdelrahmanAlsadder <aboodalsadder@gmail.com>
2024-07-18 23:33:23 +0300

    changed table name

--- src/layouts/cases/index.js
+++ src/layouts/cases/index.js
@@ -51,7 +51,7 @@ function Tables() {
                 coloredShadow="info"
               >
                 <MDTypography variant="h6" color="white">
-                  Authors Table
+                  Dispute Table
                 </MDTypography>
               </MDBox>
               <MDBox pt={3}>
