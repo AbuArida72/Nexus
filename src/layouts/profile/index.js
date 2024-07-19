2b5dc0100029b9749191f2e98b866fc1fb2eeac4
Mohammad Abu Arida <74688331+AbuArida72@users.noreply.github.com>
2024-07-19 14:43:29 +0300

    Update index.js

--- src/layouts/profile/index.js
+++ src/layouts/profile/index.js
@@ -50,7 +50,38 @@ function Tables() {
                 coloredShadow="info"
               >
                 <MDTypography variant="h6" color="white">
-                  Dispute Table
+                  Sent Disputes
+                </MDTypography>
+              </MDBox>
+              <MDBox pt={3}>
+                <DataTable
+                  table={{ columns, rows }}
+                  isSorted={false}
+                  entriesPerPage={false}
+                  showTotalEntries={false}
+                  noEndBorder
+                />
+              </MDBox>
+            </Card>
+          </Grid>
+        </Grid>
+      </MDBox>
+      <MDBox pt={6} pb={3}>
+        <Grid container spacing={6}>
+          <Grid item xs={12}>
+            <Card>
+              <MDBox
+                mx={2}
+                mt={-3}
+                py={3}
+                px={2}
+                variant="gradient"
+                bgColor="info"
+                borderRadius="lg"
+                coloredShadow="info"
+              >
+                <MDTypography variant="h6" color="white">
+                  Recieved Disputes
                 </MDTypography>
               </MDBox>
               <MDBox pt={3}>
